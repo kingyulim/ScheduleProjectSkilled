0c684380ef0ea511e39a2ead342fe6ca97e506b5
Kingyul Kim <youremail@example.com>
2025-11-11 16:22:56 +0900

    Initial commit

---
+++ src/main/java/com/scheduleprojectskilled/ScheduleProjectSkilledApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleprojectskilled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectSkilledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleProjectSkilledApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scheduleprojectskilled/ScheduleProjectSkilledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleprojectskilled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectSkilledApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
