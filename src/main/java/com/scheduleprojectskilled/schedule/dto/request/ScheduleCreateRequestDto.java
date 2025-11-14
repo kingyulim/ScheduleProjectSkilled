f1b3646691723f43018c07f44fe4503f6f872803
Kingyul Kim <youremail@example.com>
2025-11-14 19:49:24 +0900

    Refactor: 스케줄 생성 request dto 코드 리팩토링 - 이름 입력 부분 삭제 세션 값으로 대처

--- src/main/java/com/scheduleprojectskilled/schedule/dto/request/ScheduleCreateRequestDto.java
+++ src/main/java/com/scheduleprojectskilled/schedule/dto/request/ScheduleCreateRequestDto.java
@@ -7,20 +7,6 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleCreateRequestDto {
-    @NotBlank(message = "이름이 입력되지 않았습니다.")
-    @Size(
-            min = 1,
-            max = 20,
-            message = "이름은 1 ~ 20자 로 입력해주세요."
-    )
-    @Pattern(
-            regexp = "^[가-힣a-zA-Z]+$",
-            message = "이름 형식이 아닙니다."
-    )
-    private String wriName;
-
-    //------------
-
     @NotBlank(message = "제목이 입력되지 않았습니다.")
     @Size(
             min = 1,
