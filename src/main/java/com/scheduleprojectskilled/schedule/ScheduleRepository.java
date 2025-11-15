2d29131dff9d6f9b6c95bbf4c523e8604ff09c5f
Kingyul Kim <youremail@example.com>
2025-11-15 11:58:04 +0900

    Feat: ScheduleRepository query methode 추가 - existsByIdAndMemberIdAndWriName = 스케줄 고유 번호와 세션에 존재하는 회원 고유 번호 이름 이 일치하면 true 반환

--- src/main/java/com/scheduleprojectskilled/schedule/ScheduleRepository.java
+++ src/main/java/com/scheduleprojectskilled/schedule/ScheduleRepository.java
@@ -8,10 +8,18 @@ import java.util.Optional;
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
     /**
      * 내가 쓴 글 고유 번호 와 고유 번호에 해당되는 이름으로 찾는 query methode
-     * @param memberId 회원 고유 번호
-     * @param wriName 회원 고유 번호와 같은 이름
+     * @param memberId 회원 고유 번호 파라미터
+     * @param wriName 회원 고유 번호와 같은 이름 파라미터
      * @return query return
      */
     List<ScheduleEntity> findByMemberIdAndWriName(Long memberId, String wriName);
 
+    /**
+     * 스케줄 고유 번호와 세션에 존재하는 회원 고유 번호 이름 이 일치하면 true 반환 query methode
+     * @param scheduleId 스케줄 고유 번호 파라미터
+     * @param memberId 세션 회원 고유 번호
+     * @param wriName 스케줄 작성자 이름
+     * @return query return
+     */
+    boolean existsByIdAndMemberIdAndWriName(Long scheduleId, Long memberId, String wriName);
 }
