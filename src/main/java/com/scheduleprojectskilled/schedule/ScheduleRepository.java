73f1fa6962e7c4d156a082789e0d2c784b1505b1
Kingyul Kim <youremail@example.com>
2025-11-15 11:20:25 +0900

    Feat(schedule): 내가 쓴 글 고유 번호 와 고유 번호에 해당되는 이름으로 찾는 query methode 추가 - findByMemberIdAndWriName

--- src/main/java/com/scheduleprojectskilled/schedule/ScheduleRepository.java
+++ src/main/java/com/scheduleprojectskilled/schedule/ScheduleRepository.java
@@ -2,5 +2,16 @@ package com.scheduleprojectskilled.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ScheduleRepository extends JpaRepository<ScheduleEntity, Long> {
+    /**
+     * 내가 쓴 글 고유 번호 와 고유 번호에 해당되는 이름으로 찾는 query methode
+     * @param memberId 회원 고유 번호
+     * @param wriName 회원 고유 번호와 같은 이름
+     * @return query return
+     */
+    List<ScheduleEntity> findByMemberIdAndWriName(Long memberId, String wriName);
+
 }
