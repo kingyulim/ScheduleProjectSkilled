3218e34a0557b656b22c0af7e84fd3a718861979
Kingyul Kim <youremail@example.com>
2025-11-17 19:43:35 +0900

    Refactor(schedule): extends ComScheduleRequestDto 연동 - 스케줄 제목 - 스케줄 내용

--- src/main/java/com/scheduleprojectskilled/schedule/dto/request/ScheduleCreateRequestDto.java
+++ src/main/java/com/scheduleprojectskilled/schedule/dto/request/ScheduleCreateRequestDto.java
@@ -1,23 +1,7 @@
 package com.scheduleprojectskilled.schedule.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
-public class ScheduleCreateRequestDto {
-    @NotBlank(message = "제목이 입력되지 않았습니다.")
-    @Size(
-            min = 1,
-            max = 50,
-            message = "제목은 1 ~ 50자 로 입력해주세요."
-    )
-    private String scheduleTitle;
-
-    //------------
-
-    @NotBlank(message = "내용이 입력되지 않았습니다.")
-    //@Pattern(regexp = "^<([a-z]+)([^<]+)(>(.)<\\/\\1>|\\s*\\/>)$")
-    private String scheduleContent;
+public class ScheduleCreateRequestDto extends ComScheduleRequestDto {
 }
