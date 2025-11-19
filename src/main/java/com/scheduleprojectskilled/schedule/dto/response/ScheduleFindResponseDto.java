22efb8df896c531f9c3465b61ae898813149cf93
Kingyul Kim <youremail@example.com>
2025-11-19 15:12:18 +0900

    Rename, Feat(schedule): 스케줄 찾기 response 파일 이름 변경 및 속성 추가 - FindScheduleResponseDto → ScheduleFindResponseDto - private final List<CommentFindResponseDto> commentsList 추가

--- src/main/java/com/scheduleprojectskilled/schedule/dto/response/ScheduleFindResponseDto.java
+++ src/main/java/com/scheduleprojectskilled/schedule/dto/response/ScheduleFindResponseDto.java
@@ -1,17 +1,21 @@
 package com.scheduleprojectskilled.schedule.dto.response;
 
+import com.scheduleprojectskilled.comment.CommentEntity;
+import com.scheduleprojectskilled.comment.Dto.Response.CommentFindResponseDto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
-public class FindScheduleResponseDto {
+public class ScheduleFindResponseDto {
     private final Long id;
     private final String wriName;
     private final String scheduleTitle;
     private final String scheduleContent;
     private final LocalDateTime createDatetime;
     private final LocalDateTime updateDatetime;
+    private final List<CommentFindResponseDto> commentsList;
 }
