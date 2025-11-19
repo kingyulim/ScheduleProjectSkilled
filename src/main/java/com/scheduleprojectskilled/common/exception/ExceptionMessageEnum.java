3619d1360900efd75b224b54c2b9be815a8dbc12
Kingyul Kim <youremail@example.com>
2025-11-19 14:38:47 +0900

    Feat(exception): 예외처리 추가 - MEMBER_FORBIDDEN(FORBIDDEN): 해당 유저는 권한이 없습니다. - NOT_FOUND_COMMENT(NOT_FOUND): 댓글을 찾을 수 없습니다. - NOT_DELETE_SCHEDULE(UNAUTHORIZED): 1개 이상의 댓글이 존재하여 삭제가 불가능 합니다. - MEMBER_DELETE_CHECK(UNAUTHORIZED): 탈퇴한 회원 입니다.

--- src/main/java/com/scheduleprojectskilled/common/exception/ExceptionMessageEnum.java
+++ src/main/java/com/scheduleprojectskilled/common/exception/ExceptionMessageEnum.java
@@ -8,9 +8,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExceptionMessageEnum {
     NO_MEMBER_ID(HttpStatus.NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다."),
+    MEMBER_FORBIDDEN(HttpStatus.FORBIDDEN, "해당 유저는 권한이 없습니다."),
     NOT_FOUND_SCHEDULE(HttpStatus.NOT_FOUND, "해당 스케줄을 찾을 수 없습니다."),
+    NOT_FOUND_COMMENT(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
+    NOT_DELETE_SCHEDULE(HttpStatus.UNAUTHORIZED, "1개 이상의 댓글이 존재하여 삭제가 불가능 합니다."),
     NO_LOGIN(HttpStatus.UNAUTHORIZED, "로그인이 되지 않았습니다."),
     MEMBER_CHECK(HttpStatus.UNAUTHORIZED, "회원정보가 맞지 않습니다."),
+    MEMBER_DELETE_CHECK(HttpStatus.UNAUTHORIZED, "탈퇴한 회원 입니다."),
     NO_SESSION(HttpStatus.UNAUTHORIZED, "세선이 조회되지 않습니다."),
     LOGIN_CHECK(HttpStatus.FORBIDDEN, "로그인 되었습니다."),
     NO_MEMBER_KEY_MATCHING(HttpStatus.UNAUTHORIZED, "회원 고유 번호가 일치하지 않습니다."),
